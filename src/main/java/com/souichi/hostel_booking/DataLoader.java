8f75582619a56e7e39b370469234c604f9f55fe0
Tang Wing Sum <404tayoz@gmail.com>
2026-05-01 13:34:06 +0900

    Enhance room creation with names and capacities
    Updated the run method to include room names and capacities.

--- src/main/java/com/souichi/hostel_booking/DataLoader.java
+++ src/main/java/com/souichi/hostel_booking/DataLoader.java
@@ -15,20 +15,25 @@ public class DataLoader implements CommandLineRunner {
     }
 
     @Override
-public void run(String... args) {
-    if (roomRepository.count() == 0) {
+    public void run(String... args) throws Exception {
+        if (roomRepository.count() == 0) {
+            Room room1 = new Room();
+            room1.setName("Single Room");
+            room1.setCapacity(1);
+            room1.setPricePerNight(5000);
+            roomRepository.save(room1);
 
-        Room room1 = new Room();
-        room1.setPricePerNight(5000);
-        roomRepository.save(room1);
+            Room room2 = new Room();
+            room2.setName("Double Room");
+            room2.setCapacity(2);
+            room2.setPricePerNight(8000);
+            roomRepository.save(room2);
 
-        Room room2 = new Room();
-        room2.setPricePerNight(8000);
-        roomRepository.save(room2);
-
-        Room room3 = new Room();
-        room3.setPricePerNight(7000);
-        roomRepository.save(room3);
-    }
+            Room room3 = new Room();
+            room3.setName("Suite");
+            room3.setCapacity(4);
+            room3.setPricePerNight(12000);
+            roomRepository.save(room3);
+        }
     }
 }
